728128aa2cd6347b78bed7849a12d85e0f1f8ab2
tomlit <tomlit@gmail.com>
2024-01-30 05:25:35 -0500

    update useCompanyData

--- hooks/useCompanyData.tsx
+++ hooks/useCompanyData.tsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 const fetcher = (url: string | URL | Request) => fetch(url).then((res) => res.json());
 
 function useCompanyData() {
-  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "";
   const { data, error } = useSWR(`${baseUrl}/api/company`, fetcher);
   return { data, error };
 }
--- lib/api.tsx
+++ lib/api.tsx
@@ -1,5 +1,5 @@
 export async function getCompany() {
-  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "";
   const response = await fetch(`${baseUrl}/api/company`);
   const companies = await response.json();
   return companies;
